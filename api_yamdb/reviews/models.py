73c3f48b9196b9160882a7fd9415ecc73f4313de
Eugen Art <artashkineug@yandex.ru>
2024-08-15 19:37:23 +0300

    fix reviews model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,30 +1,57 @@
+from django.db import models
 
-class Genres(models.Model):
-    name = models.CharField(max_length=256)
-    slug = models.SlugField(max_length=50)
 
-class GenresTitles(models.Model):
-    title_id = models.ForeignKey(Genres,
+class Category(models.Model):
+    name = models.CharField('Название категории', max_length=256)
+    slug = models.SlugField('Идентификатор', max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField('Название жанра', max_length=256)
+    slug = models.SlugField('Идентификатор', max_length=50, unique=True)
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+    def __str__(self):
+        return self.name
+
+
+class TitleGenre(models.Model):
+    title_id = models.ForeignKey('Title',
                                  on_delete=models.SET_NULL, null=True,)
-    genre_id =  models.ForeignKey('Titles',
-                                  on_delete=models.SET_NULL, null=True,)    
+    genre_id = models.ForeignKey(Genre,
+                                 on_delete=models.SET_NULL, null=True,)
 
-class Reviews(models.Model):
-    pass
 
-class Comments(models.Model):
-    pass
-
-class Titles(models.Model):
-    name = models.CharField(max_length=256)
-    description = models.TextField()
-    year = models.IntegerField()
+class Title(models.Model):
+    name = models.CharField('Название произведения', max_length=256)
+    description = models.TextField('Описание')
+    year = models.IntegerField('Год')
     category = models.ForeignKey(
-        Categories,
+        Category,
         on_delete=models.SET_NULL, null=True,
+        verbose_name='Категория',
     )
-    rewiew = models.OneToOneField(
-        Reviews,  
-        on_delete=models.SET_NULL, null=True,
-    ) #в документации не увидела. нужен ли?
-    #raiting = не поняла
+    genre = models.ManyToManyField(Genre, through=TitleGenre,
+                                   verbose_name='Жанр',)  # сомневаюсь в этом поле
+    # rewiew = models.ForeignKey(
+    #    Review,
+    #    on_delete=models.SET_NULL, null=True,
+    # )  в документации не увидела. нужен ли?
+    raiting = models.FloatField('Рейтинг', null=True)
+
+    class Meta:
+        verbose_name = 'Произведение'
+        verbose_name_plural = 'Произведения'
+
+    def __str__(self):
+        return self.name
